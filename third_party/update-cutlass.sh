c7866cbd60b1f219e73b4958f63c6843c072bae8
shiyi0113 <shiyileshengxia@gmail.com>
2025-05-22 19:25:34 +0800

    environmental configuration

---
+++ third_party/update-cutlass.sh
@@ -0,0 +1,17 @@
+set -xe
+
+# cutlass tag
+TAG_NAME="v3.9.2" 
+
+rm -rf cutlass.origin
+
+git clone --branch "$TAG_NAME" --single-branch https://github.com/NVIDIA/cutlass.git cutlass.origin
+
+rm -rf cutlass
+mkdir -p cutlass
+cp -r cutlass.origin/include cutlass
+
+echo "CUTLASS version: $TAG_NAME" > cutlass/readme
+git -C cutlass.origin/ rev-parse "$TAG_NAME" >> cutlass/readme 
+
+rm -rf cutlass.origin
