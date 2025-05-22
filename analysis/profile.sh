f7cbf761cd28d6aa5010ee184b11fb39a0c0acf4
shiyi0113 <shiyileshengxia@gmail.com>
2025-05-22 19:59:20 +0800

    create vectorAdd_cute

--- analysis/profile.sh
+++ analysis/profile.sh
@@ -1,4 +1,4 @@
 ncu=`which ncu`
-sudo ${ncu} --csv --log-file a.csv --cache-control=all --clock-control=base --metrics gpu__time_duration.sum ./gemm-multi-stage
+sudo ${ncu} --csv --log-file a.csv --cache-control=all --clock-control=base --metrics gpu__time_duration.sum ../build/cute_gemm
 
 python3 stat-csv.py
